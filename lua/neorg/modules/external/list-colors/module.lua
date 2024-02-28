2ac622f0c103e5280ddec60f89c52b0dd67cd887
dane <dhcjensen@gmail.com>
2024-02-27 23:22:48 -0800

    figured out the issue. it works now. ready to start.

--- lua/neorg/modules/external/list-colors/module.lua
+++ lua/neorg/modules/external/list-colors/module.lua
@@ -1,9 +1,13 @@
-local neorg = require("neorg.modules.base")
+local neorg = require("neorg.core")
 
 local module = neorg.modules.create("external.list-colors")
 
+module.config.public = {
+	name = "World",
+}
+
 module.load = function()
-	print("Hello World!")
+	print("Hello " .. module.config.public.name .. "!")
 end
 
 return module
